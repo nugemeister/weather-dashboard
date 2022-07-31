edeab6d1c1c4552018674de0077a043f373838bb
Ali Nugen <alexandra.nugen@gmail.com>
2022-07-31 13:25:06 -0700

    adding event listener and DOM components for searching on home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,46 @@
 
 // Universal Variables
 
+// Search Form Variables
+var userCityInputEl = $("#userCityInput");
+var userCitySearchButtonEl = $("#userCitySearchButton");
+// PLACEHOLDER FOR STATE LIST EL (TBC)
+
+// Weather Display Variables
+var searchHistoryEl = $("#searchHistory");
+var currentWeatherEl = $("#currentWeather");
+var forecastWeatherEl = $("#forecastWeather");
+
+// API Info
+var apiKey = "ddaeddc476ffe0c412f2543004ba0f76";
+var apiURL = "https://api.openweathermap.org/data/2.5/onecall";
+
+// Historical Search Info
+var searchHistoryList = [];
+
+// Initial Page Load
+// Pulling History from Local Storage
+localStorage.getItem('search-history') {
+    searchHistoryList = JSON.parse(localStorage.getItem('search-history'));
+    console.log(searchHistoryList);
+}
+// Rendering History from Local Storage
+searchHistoryEl.html(rendorHistory());
+console.log(searchHistoryEl);
+
+// Event Listener for Initial City Search
+userCitySearchButtonEl.on("click", function(event) {
+    event.preventDefault();
+    // No blank searches allowed, prompt to ask user to populate city
+    if (userCityInputEl.val() === "") {
+        displayModal("Please enter a city to begin searching!");
+        return;
+    // } else {
+    //     catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+    }});
+
+
+
